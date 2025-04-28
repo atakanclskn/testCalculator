f2e6b9e51175d5d50eebb99ddff505388fd4ae31
Atakan Çalışkan <atakanclskn@outlook.com>
2025-04-28 22:56:37 +0300

    test

---
+++ testCalculatorConsole/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testCalculatorConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //@atakanclskn
+            //@mericozkaya
+        }
+    }
+}
